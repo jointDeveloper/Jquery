9029266116a63e37148f546a32ea12ae0c4c0219
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-07-02 20:39:41 -0500

    Se modifica la intro

--- introduccion/js/intro.js
+++ introduccion/js/intro.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
-  // selectores
-  // .clase
-  // #id
-  // elemento: h1, p, div, li, ...
-
-  // addClass, removeClass, toggleClass, attr
-  //alert($('h3').css('color'));
-  //$('h3').css('color', '#1aa673');
-  var id = '#' + $('.prueba').attr('id');
-  $(id).css('background-color', '#FF3');
-  // alert($('h3').css('color'));
-
-  // filtros
-  // :eq(), :lt(), :gt(), :has()
-  $('li:nth-child(odd)').css('color', 'blue');
-  $('li:gt(2)').css('color', 'orange');
-  $('li:has(h4)').css('color', 'red');
-
-  // text, html
-  //alert($('h3').text());
-  //$('h3').text("Modificando texto desde jQuery");
-  //$('h3').html("Creando html desde <i>jQuery</i>");
-});<
+  $('h3').html('Modificando el texto con <i>JQ<i>');
+  $('#jq').addClass('otraclase').removeClass('miclase');
+  $('ul').siblings().css('border', 'solid yellow');
+  $('li').parent().css('border', 'solid red');
+  $('li:eq(1)').css('color', "#F5E");
+  $('li:lt(2)').css('background', "green");
+  $('li:gt(2)').css('background', "gray");
+  $('li:has(p)').css('color', "#FBD");
+});
